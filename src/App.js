e53c2a8c33854ac72728ad18f9480040051dc53d
nicolas <thelastnicko@gmail.com>
2022-03-12 11:04:41 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.scss";
 import Header from "./components/Header/Header.js";
+import Main from "./components/Main/Main";
+import "./components/Main/Main.scss";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Switch></Switch>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/upload" component={VideoUploadPage} />
+          <Route path="/:videoId" component={Main} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const VideoUploadPage = () => {
+  return <div>VideoUploadPage</div>;
+};
